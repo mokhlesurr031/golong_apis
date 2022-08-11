83d54085e5566e4c967586483399e34389a25d62
mokhlesurr031 <mokhlesurr031@gmail.com>
2022-08-11 13:25:34 +0600

    [ADD] Basic handler golang

--- main.go
+++ main.go
@@ -1,60 +1,45 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 )
 
-type Book struct {
-	ID     string `json:"id"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
+const Port = ":8000"
+
+type Article struct {
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
 }
 
-var books = []Book{
-	{ID: "1", Title: "Harry Potter", Author: "J. K. Rowling"},
-	{ID: "2", Title: "The Lord of the Rings", Author: "J. R. R. Tolkien"},
-	{ID: "3", Title: "The Wizard of Oz", Author: "L. Frank Baum"},
+var Articles []Article
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL.Path)
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL.Path)
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func requestHandler() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", returnAllArticles)
+
+	log.Fatal(http.ListenAndServe(Port, nil))
 }
 
 func main() {
-	r := gin.New()
-
-	r.GET("/", listBooksHandler)
-	r.POST("/books", createBookHandler)
-	r.DELETE("/books/:id", deleteBookHandler)
-
-	r.Run()
-}
-
-func listBooksHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, books)
-}
-
-func createBookHandler(c *gin.Context) {
-	var book Book
-
-	if err := c.ShouldBindJSON(&book); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
+	Articles = []Article{
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
 	}
 
-	books = append(books, book)
+	requestHandler()
 
-	c.JSON(http.StatusCreated, book)
-}
-
-func deleteBookHandler(c *gin.Context) {
-	id := c.Param("id")
-
-	for i, a := range books {
-		if a.ID == id {
-			books = append(books[:i], books[i+1:]...)
-			break
-		}
-	}
-
-	c.Status(http.StatusNoContent)
 }
